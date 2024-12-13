5f1cdb74b5fb6f7c78ea3e9b4c75c93138be71cf
asm-dev <salamanca.m@outlook.com>
2024-12-13 21:37:05 +0000

    fix issues on url and view

--- spotifyapiproject/songs/urls.py
+++ spotifyapiproject/songs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SongListCreateView
 
 urlpatterns = [
-    path('songs/',SongListCreateView.as_view, name="song-list")
+    path('songs/', SongListCreateView.as_view(), name="song-list")
 ]
--- spotifyapiproject/songs/views.py
+++ spotifyapiproject/songs/views.py
@@ -5,3 +5,4 @@ from .serialializers import SongSerializer
 class SongListCreateView(generics.ListCreateAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
