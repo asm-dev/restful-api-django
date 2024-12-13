2156330cb72fa8c31e128c581b89f9a4b0ab81e9
asm-dev <salamanca.m@outlook.com>
2024-12-13 20:52:03 +0000

    create model and song serializer

--- spotifyapiproject/songs/models.py
+++ spotifyapiproject/songs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    artist = models.CharField(max_length=255, blank=True)
+    album = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title()
