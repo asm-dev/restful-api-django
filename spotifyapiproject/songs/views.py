2e1610d54818e4d4aadff8d2bb9172e64dfa8aef
asm-dev <salamanca.m@outlook.com>
2024-12-13 21:09:45 +0000

    create songs view

--- spotifyapiproject/songs/views.py
+++ spotifyapiproject/songs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Song
+from .serialializers import SongSerializer
 
-# Create your views here.
+class SongListCreateView(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
